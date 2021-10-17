cde981b2dc6d66fa28475da0105424a34ceed6ea
Vidhi Patel <vidhibpatel94@gmail.com>
2021-10-17 18:54:17 -0400

    Fix: UI

--- vastrafashiondesigner/src/main/java/com/esolution/vastrafashiondesigner/ui/products/CatalogueProductsActivity.java
+++ vastrafashiondesigner/src/main/java/com/esolution/vastrafashiondesigner/ui/products/CatalogueProductsActivity.java
@@ -1,14 +1,19 @@
 package com.esolution.vastrafashiondesigner.ui.products;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.esolution.vastrafashiondesigner.R;
 import com.esolution.vastrafashiondesigner.databinding.ActivityCatalogueProductsBinding;
+import com.esolution.vastrafashiondesigner.databinding.LayoutToolbarMenuItemBinding;
+import com.esolution.vastrafashiondesigner.ui.newproduct.AddProductInfo1Activity;
 
 public class CatalogueProductsActivity extends AppCompatActivity {
 
@@ -20,11 +25,27 @@ public class CatalogueProductsActivity extends AppCompatActivity {
         binding = ActivityCatalogueProductsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.toolbarLayout.title.setText(R.string.catalogue_name);
-        binding.toolbarLayout.iconBack.setOnClickListener(v -> onBackPressed());
+        initToolbar();
 
         ProductAdapter adapter = new ProductAdapter();
         binding.productsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         binding.productsRecyclerView.setAdapter(adapter);
+
+        binding.btnAdd.setOnClickListener((v) -> {
+            Intent intent = new Intent(CatalogueProductsActivity.this, AddProductInfo1Activity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void initToolbar() {
+        binding.toolbarLayout.title.setText(R.string.catalogue_name);
+        binding.toolbarLayout.iconBack.setOnClickListener(v -> onBackPressed());
+
+        binding.toolbarLayout.layoutMenu.setVisibility(View.VISIBLE);
+        LayoutToolbarMenuItemBinding menuItemBinding = LayoutToolbarMenuItemBinding.inflate(getLayoutInflater());
+        binding.toolbarLayout.layoutMenu.addView(menuItemBinding.getRoot());
+
+        menuItemBinding.actionMenuItem.setImageDrawable(ContextCompat.getDrawable(this,
+                R.drawable.ic_search_grey_500_24dp));
     }
 }
