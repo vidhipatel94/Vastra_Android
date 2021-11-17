ea9de8c3c8e4eb3d5c931bf2534fa3b236c50173
Vandana Ghori <vandanaghori19@gmail.com>
2021-11-16 20:42:33 -0500

    Show Designers list dynamically for shoppers.

--- vastrashopper/src/main/java/com/esolution/vastrashopper/ui/designers/DesignersAdapter.java
+++ vastrashopper/src/main/java/com/esolution/vastrashopper/ui/designers/DesignersAdapter.java
@@ -1,6 +1,7 @@
 package com.esolution.vastrashopper.ui.designers;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +9,21 @@ import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.esolution.vastrabasic.models.Designer;
 import com.esolution.vastrashopper.databinding.RowDesignerBinding;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class DesignersAdapter extends RecyclerView.Adapter<DesignersAdapter.ViewHolder> {
+
+    private final List<Designer> designersList;
+
+    public DesignersAdapter(List<Designer> designersList) {
+        this.designersList = designersList;
+    }
 
     @NotNull
     @Override
@@ -22,19 +33,26 @@ public class DesignersAdapter extends RecyclerView.Adapter<DesignersAdapter.View
 
     @Override
     public void onBindViewHolder(@NotNull DesignersAdapter.ViewHolder holder, int position) {
+        Designer designer = designersList.get(position);
+
         Context context = holder.binding.getRoot().getContext();
+
+        holder.binding.textViewFDName.setText(designer.getFirstName().concat(" " + designer.getLastName()));
+        holder.binding.textViewTagLine.setText(designer.getTagline());
+        Glide.with(context).load(designer.getAvatarURL()).centerCrop()
+                .into(holder.binding.imageProfilePic);
 
         holder.binding.parentLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Fashion Designer " + position + " Selected.", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(v.getContext(), "Fashion Designer " + position + " Selected.", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return 15;
+        return designersList.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
--- vastrashopper/src/main/java/com/esolution/vastrashopper/ui/designers/DesignersFragment.java
+++ vastrashopper/src/main/java/com/esolution/vastrashopper/ui/designers/DesignersFragment.java
@@ -1,6 +1,7 @@
 package com.esolution.vastrashopper.ui.designers;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,37 +15,91 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.esolution.vastrabasic.ProgressDialogHandler;
+import com.esolution.vastrabasic.apis.RestUtils;
+import com.esolution.vastrabasic.models.Designer;
+import com.esolution.vastrashopper.R;
 import com.esolution.vastrashopper.databinding.FragmentDesignersBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import static com.esolution.vastrabasic.utils.Utils.showMessage;
 
 public class DesignersFragment extends Fragment {
 
-    private DesignersViewModel designersViewModel;
+    private final CompositeDisposable subscriptions = new CompositeDisposable();
+
+    //private DesignersViewModel designersViewModel;
     private FragmentDesignersBinding binding;
     private DesignersAdapter designersAdapter;
     private RecyclerView designerRecyclerView;
+    private final List<Designer> designersList = new ArrayList<>();
+
+    private ProgressDialogHandler progressDialogHandler;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        designersViewModel =
-                new ViewModelProvider(this).get(DesignersViewModel.class);
+        /*designersViewModel =
+                new ViewModelProvider(this).get(DesignersViewModel.class);*/
 
         binding = FragmentDesignersBinding.inflate(inflater, container, false);
 
         initView();
         bindRecyclerViewAdapter();
 
+        if(getActivity() != null) {
+            progressDialogHandler = new ProgressDialogHandler(getActivity());
+        }
+
+        getDesigners();
+
         return binding.getRoot();
     }
 
     private void initView() {
         designerRecyclerView = binding.designerRecyclerView;
-        designersAdapter = new DesignersAdapter();
+        designersAdapter = new DesignersAdapter(designersList);
     }
 
     private void bindRecyclerViewAdapter() {
-        designerRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        designerRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false));
         designerRecyclerView.setAdapter(designersAdapter);
         designersAdapter.notifyDataSetChanged();
+    }
+
+    private void getDesigners() {
+        progressDialogHandler.setProgress(true);
+        subscriptions.add(RestUtils.getAPIs().getDesigners()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(response -> {
+            progressDialogHandler.setProgress(false);
+            if(response.isSuccess()){
+                boolean success = true;
+                if(response.getData() == null) {
+                    success = false;
+                } else {
+                    designersList.clear();
+                    designersList.addAll(response.getData());
+                    designersAdapter.notifyDataSetChanged();
+                }
+                if(!success) {
+                    showMessage(binding.getRoot(), getString(R.string.server_error));
+                }
+            } else {
+                showMessage(binding.getRoot(), response.getMessage());
+            }
+        }, throwable -> {
+            progressDialogHandler.setProgress(false);
+            String message = RestUtils.processThrowable(getContext(), throwable);
+            showMessage(binding.getRoot(), message);
+        }));
     }
 
     @Override
